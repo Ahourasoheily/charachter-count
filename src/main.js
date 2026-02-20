e1d77d589d93b7813b56db16e2e750daa24c3fd0
ahourasoheily <Ahourasoheily2007@gamil.com>
2026-02-20 09:06:10 +0330

    first

---
+++ src/main.js
@@ -0,0 +1,20 @@
+console.log("hi");
+
+const inputTextE1 = document.getElementById("textinput");
+const countE1 = document.getElementById("count");
+const progressbarE1 = document.getElementById("progressbar");
+
+console.log(inputTextE1);
+// console.log(countE1);
+// console.log(progressbarE1);
+
+inputTextE1.addEventListener("input", (event) => {
+  const textlength = event.target.value.length;
+  countE1.innerHTML = textlength;
+
+  progressbarE1.style.width = textlength * 2.8 + "%";
+
+  if (textlength >= 1 && textlength <= 222) {
+    progressbarE1.style.backgroundColor = "blue";
+  }
+});
